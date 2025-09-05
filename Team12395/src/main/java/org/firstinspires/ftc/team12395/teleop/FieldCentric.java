264b99e7b4017688ba2ce8c141f8084e2f7920a1
dante <danneallstar5@gmail.com>
2025-09-04 21:20:16 -0500

    preMade Arm-code

---
+++ Team12395/src/main/java/org/firstinspires/ftc/team12395/teleop/FieldCentric.java
@@ -0,0 +1,105 @@
+/* Copyright (c) 2022 FIRST. All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without modification,
+ * are permitted (subject to the limitations in the disclaimer below) provided that
+ * the following conditions are met:
+ *
+ * Redistributions of source code must retain the above copyright notice, this list
+ * of conditions and the following disclaimer.
+ *
+ * Redistributions in binary form must reproduce the above copyright notice, this
+ * list of conditions and the following disclaimer in the documentation and/or
+ * other materials provided with the distribution.
+ *
+ * Neither the name of FIRST nor the names of its contributors may be used to endorse or
+ * promote products derived from this software without specific prior written permission.
+ *
+ * NO EXPRESS OR IMPLIED LICENSES TO ANY PARTY'S PATENT RIGHTS ARE GRANTED BY THIS
+ * LICENSE. THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+ * "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO,
+ * THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE
+ * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+ * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+ * SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+ * CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+ * OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+ * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+
+package org.firstinspires.ftc.team12395.teleop; // TODO(STUDENTS): Change to your team package (e.g., org.firstinspires.ftc.team12345.teleop)
+
+import com.acmerobotics.dashboard.config.Config;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.util.Range;
+import org.firstinspires.ftc.team12395.RobotHardware;
+
+@TeleOp(name="Field Centric", group="TeleOp")
+@Config
+// TODO(STUDENTS): You may rename this for your robot (e.g., "Field Centric - Comp Bot)
+public class FieldCentric extends LinearOpMode {
+
+    // NOTE: One hardware instance per OpMode keeps mapping/IMU use simple and testable
+    RobotHardware robot = new RobotHardware(this);
+
+    public static double baseBarrier = -1; // TODO placeholder, should be where elbow starts hitting the ground.
+    public static double barrierSlope = 1; // TODO placeholder, should be the slope where elbow starts hitting ground and base being straigh out.
+    public static double basePos, elbowPos, pinchPos  = 0; // TODO placeholder, set correct starting pos and put in INIT
+
+    @Override
+    public void runOpMode() {
+        // Driver inputs (range roughly [-1, 1])
+        double axial    = 0; // forward/back (+ forward)
+        double lateral  = 0; // strafe left/right (+ right)
+        double yaw      = 0; // rotation (+ CCW/left turn)
+
+
+
+        robot.init();
+
+        waitForStart();
+
+        // --- TELEOP LOOP ---
+        while (opModeIsActive()) {
+
+            axial   = -gamepad1.left_stick_y;
+            lateral =  gamepad1.left_stick_x;
+            yaw     =  gamepad1.right_stick_x;
+
+            robot.driveFieldCentric(axial, lateral, yaw);
+
+            // + goes up
+            basePos  += 0.25*(gamepad1.right_trigger - gamepad1.left_trigger);
+            elbowPos += (gamepad1.right_bumper ? 0.07 : 0) - (gamepad1.left_bumper ? 0.07 : 0);
+            if (gamepad1.bWasPressed()){
+                if (pinchPos != 0){
+                    pinchPos = 0;
+                }
+                else {
+                    pinchPos = 1;
+                }
+            }
+
+            basePos = Range.clip(basePos, 0, 1);
+            elbowPos = Range.clip(elbowPos, 0 ,1);
+            if (basePos <= baseBarrier){
+                elbowPos = basePos*barrierSlope;
+            }
+
+            robot.setBaseServo(basePos);
+            robot.setElbowPos(elbowPos);
+            robot.setPinchPos(pinchPos);
+
+            // Telemetry for drivers + debugging
+            telemetry.addData("Controls", "Drive/Strafe: Left Stick | Turn: Right Stick");
+            telemetry.addData("Inputs", "axial=%.2f   lateral=%.2f   yaw=%.2f", axial, lateral, yaw);
+            // telemetry.addData("Heading(rad)", robot.getHeadingRadians()); / add a getter in RobotHardware if desired
+            telemetry.update();
+
+            // Pace loop-helps with readability and prevents spamming the DS
+            sleep(50); // ~20 Hz;
+        }
+    }
+}
+
