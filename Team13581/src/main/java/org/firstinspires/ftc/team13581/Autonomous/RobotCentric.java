2a7161d7e530a51fe7e4fc15165da03fa6927de0
Chris Lemoine <chrismlemoine@gmail.com>
2025-09-09 16:43:15 -0500

    13581 Base

---
+++ Team13581/src/main/java/org/firstinspires/ftc/team13581/Autonomous/RobotCentric.java
@@ -0,0 +1,41 @@
+package org.firstinspires.ftc.team13581.Autonomous;
+
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import org.firstinspires.ftc.team13581.RobotHardware;
+
+@Autonomous(name="Robot Centric", group="Autonomous")
+
+public class RobotCentric extends LinearOpMode {
+
+    RobotHardware robot = new RobotHardware(this);
+
+    @Override
+    public void runOpMode(){
+
+        final double AXIAL_SPEED       = 0.4;
+        final double LATERAL_SPEED     = 0.4;
+        final double YAW_SPEED         = 0.2;
+
+        robot.init();
+
+        waitForStart();
+
+        while(opModeIsActive()) {
+
+            robot.autoRobotCentric(AXIAL_SPEED, 24.0, 0.0);
+            robot.turnToHeading(YAW_SPEED, -45.0);
+            robot.holdHeading(YAW_SPEED, -45.0, 0.5);
+
+            robot.autoRobotCentric(AXIAL_SPEED, 17.0, -45.0);
+            robot.turnToHeading(YAW_SPEED, 45.0);
+            robot.holdHeading(YAW_SPEED, 45, 0.5);
+
+            robot.autoRobotCentric(AXIAL_SPEED, 17.0, 45.0);
+            robot.turnToHeading(YAW_SPEED, 0.0);
+            robot.holdHeading(YAW_SPEED, 0.0, 1.0);
+
+            robot.autoRobotCentric(AXIAL_SPEED, -48.0, 0.0);
+        }
+    }
+}
