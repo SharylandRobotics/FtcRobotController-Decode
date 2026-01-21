3ef5113ffd5f231b0ce047347bf95bae0dda753e
dante <danneallstar5@gmail.com>
2026-01-21 01:30:03 -0600

    autoPath

--- MeepMeepTesting/src/main/java/team12395/MeepMeepTesting.java
+++ MeepMeepTesting/src/main/java/team12395/MeepMeepTesting.java
@@ -11,15 +11,11 @@ import java.time.temporal.Temporal;
 public class MeepMeepTesting {
     public static void main(String[] args) {
         MeepMeep meepMeep = new MeepMeep(700);
-        Pose2d example = new Pose2d(-20, -40, Math.toRadians(25));
         /*
+        Pose2d example = new Pose2d(-20, -40, Math.toRadians(25));
 
         double cameraOffset = 5;
         double turretAngle = -45;
-
-
-
-
 
         // camera-turret axis offset
         example = example.plus(new Twist2d(new Vector2d(-cameraOffset,0), Math.toRadians(0)));
@@ -32,22 +28,67 @@ public class MeepMeepTesting {
         // turret axis-bot center offset
         example = example.plus(new Twist2d(new Vector2d(10,0), Math.toRadians(0)));
 
-         */
-
         Pose2d target = new Pose2d(-70, 56, Math.toRadians(0));
         double deg = Math.atan2(target.position.y - example.position.y,
                 target.position.x - example.position.x) - example.heading.imag;
+         */
+        Pose2d startPose = new Pose2d(-61.885, 37, Math.toRadians(90));
 
+        Pose2d shoot1 =  new Pose2d(-16, 28, Math.toRadians(90));
+
+        Pose2d preIntake1 = new Pose2d(-11, 28, Math.toRadians(90));
+        Pose2d postIntake1 = new Pose2d(preIntake1.position.x, 55, Math.toRadians(90));
+
+        Pose2d openGate = new Pose2d(-1, 55, Math.toRadians(90));
+
+        Pose2d preIntake2 = new Pose2d(12, 28, Math.toRadians(90));
+        Pose2d postIntake2 = new Pose2d(preIntake2.position.x, postIntake1.position.y, Math.toRadians(90));
+
+        Pose2d preIntake3 = new Pose2d(36, 28, Math.toRadians(90));
+        Pose2d postIntake3 = new Pose2d(preIntake3.position.x, postIntake1.position.y, Math.toRadians(90));
 
 
         RoadRunnerBotEntity myBot = new DefaultBotBuilder(meepMeep)
                 // Set bot constraints: maxVel, maxAccel, maxAngVel, maxAngAccel, track width
-                .setConstraints(60, 60, Math.toRadians(180), Math.toRadians(180), 15)
+                .setConstraints(60, 60, Math.toRadians(180), Math.toRadians(180), 16.223)
                 .build();
 
-                myBot.runAction(myBot.getDrive().actionBuilder(example)
-                                .turn(deg)
-                                .splineToConstantHeading(target.position, deg + example.heading.imag)
+                myBot.runAction(myBot.getDrive().actionBuilder(startPose)
+                                .setTangent(Math.atan2(shoot1.position.y - startPose.position.y, shoot1.position.x - startPose.position.x))
+                                .lineToX(shoot1.position.x)
+                        // shoot
+                                .setTangent(0)
+                                .lineToX(preIntake1.position.x)
+                        //start intake
+                                .setTangent(Math.toRadians(90))
+                                .lineToY(postIntake1.position.y)
+                        // stop intake
+
+                                .setTangent(Math.toRadians(-90))
+                                .splineToConstantHeading(openGate.position, Math.toRadians(90))
+                            .setTangent(Math.atan2(shoot1.position.y - openGate.position.y, shoot1.position.x - openGate.position.x))
+                        // wait for gate
+
+                                //.setTangent(Math.atan2(shoot1.position.y - postIntake1.position.y, shoot1.position.x - postIntake1.position.x))
+                                .lineToX(shoot1.position.x)
+                        // shoot
+                                .setTangent(0)
+                                .lineToX(preIntake2.position.x)
+                        // start intake
+                                .setTangent(Math.toRadians(90))
+                                .lineToY(postIntake2.position.y)
+                        // stop intake
+                                .setTangent(Math.atan2(shoot1.position.y - postIntake2.position.y, shoot1.position.x - postIntake2.position.x))
+                                .lineToX(shoot1.position.x)
+                        // shoot
+                                .setTangent(0)
+                                .lineToX(preIntake3.position.x)
+                        // start intake
+                                .setTangent(Math.toRadians(90))
+                                .lineToY(postIntake3.position.y)
+                        // stop intake
+                                .setTangent(Math.atan2(shoot1.position.y - postIntake3.position.y, shoot1.position.x - postIntake3.position.x))
+                                .lineToX(shoot1.position.x)
                         .build());
 
 
